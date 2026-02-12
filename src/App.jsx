f279dd622608b1cf4a9028d08a6909963e09404b
Tristan Cravello <tlcravello@gmail.com>
2026-02-12 14:14:03 -0500

    Initial commit of Pokedex

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Pokegame from './Pokegame'; 
+// import './App.css';
+
+function App () {
+    return (
+        <div className="App">
+            <h1>Pokegame</h1>
+            <Pokegame />
+        </div>
+    );
+}
+
+export default App;
