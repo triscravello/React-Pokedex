cb5b41375004e25053c2fe5008a8c544feaef0d2
Tristan Cravello <tlcravello@gmail.com>
2026-03-03 11:46:53 -0500

    Add refresh button to roll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Pokegame from './Pokegame'; 
 // import './App.css';
 
 function App () {
+    const [gameId, setGameId] = useState(0);
+
+    function refreshHand() {
+        setGameId(prev => prev + 1);
+    }
+
     return (
         <div className="App">
             <h1>Pokegame</h1>
-            <Pokegame />
+            <button onClick={refreshHand}>
+                🎲 Roll Again
+            </button>
+
+            {/* key forces full rerender */}
+            <Pokegame key={gameId} />
         </div>
     );
 }
